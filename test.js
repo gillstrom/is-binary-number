8a7d10dcbdd47d100426d84bb1c127ca18f28459
Andreas Gillström <andreasgillstrom@gmail.com>
2016-01-12 18:23:17 +0100

    Test with `AVA`

--- test.js
+++ test.js
@@ -1,40 +1,39 @@
-'use strict';
-var assert = require('assert');
-var isBinaryNumber = require('./');
+import test from 'ava';
+import m from './';
 
-it('should return true using those strings', function () {
-	assert.strictEqual(isBinaryNumber('0'), true);
-	assert.strictEqual(isBinaryNumber('1'), true);
-	assert.strictEqual(isBinaryNumber('11'), true);
-	assert.strictEqual(isBinaryNumber('1001'), true);
-	assert.strictEqual(isBinaryNumber('1111111'), true);
-	assert.strictEqual(isBinaryNumber('10000000000'), true);
-	assert.strictEqual(isBinaryNumber('101110101100000'), true);
+test('should return true using those strings', t => {
+	t.true(m('0'));
+	t.true(m('1'));
+	t.true(m('11'));
+	t.true(m('1001'));
+	t.true(m('1111111'));
+	t.true(m('10000000000'));
+	t.true(m('101110101100000'));
 });
 
-it('should return false using those strings', function () {
-	assert.strictEqual(isBinaryNumber('01'), false);
-	assert.strictEqual(isBinaryNumber('abc'), false);
-	assert.strictEqual(isBinaryNumber('1113'), false);
-	assert.strictEqual(isBinaryNumber('11011%'), false);
-	assert.strictEqual(isBinaryNumber('0001'), false);
-	assert.strictEqual(isBinaryNumber('0000'), false);
+test('should return false using those strings', t => {
+	t.false(m('01'));
+	t.false(m('abc'));
+	t.false(m('1113'));
+	t.false(m('11011%'));
+	t.false(m('0001'));
+	t.false(m('0000'));
 });
 
-it('should return true using those numbers', function () {
-	assert.strictEqual(isBinaryNumber(0), true);
-	assert.strictEqual(isBinaryNumber(1), true);
-	assert.strictEqual(isBinaryNumber(11), true);
-	assert.strictEqual(isBinaryNumber(1001), true);
-	assert.strictEqual(isBinaryNumber(1111111), true);
-	assert.strictEqual(isBinaryNumber(10000000000), true);
-	assert.strictEqual(isBinaryNumber(101110101100000), true);
+test('should return true using those numbers', t => {
+	t.true(m(0));
+	t.true(m(1));
+	t.true(m(11));
+	t.true(m(1001));
+	t.true(m(1111111));
+	t.true(m(10000000000));
+	t.true(m(101110101100000));
 });
 
-it('should return false using those numbers', function () {
-	assert.strictEqual(isBinaryNumber(1113), false);
-	assert.strictEqual(isBinaryNumber(2), false);
-	assert.strictEqual(isBinaryNumber(58159), false);
-	assert.strictEqual(isBinaryNumber(1001004414101), false);
-	assert.strictEqual(isBinaryNumber(NaN), false);
+test('should return false using those numbers', t => {
+	t.false(m(1113));
+	t.false(m(2));
+	t.false(m(58159));
+	t.false(m(1001004414101));
+	t.false(m(NaN));
 });
